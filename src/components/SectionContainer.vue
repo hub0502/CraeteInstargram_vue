6892789b816fbe8d7c3098a5c2d04c77ac650c55
leedohyeon0502 <leedohyeon0502@gmail.com>
2022-08-19 15:00:31 +0900

    데이터 바인딩

---
+++ src/components/SectionContainer.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <Post v-for="(user, key) in userList" :key="key" :userData="user"/>
+
+    </div>
+</template>
+
+<script>
+import Post from './ArticlePost.vue'
+export default {
+    components:{
+        Post,
+    },
+    props:{
+        userList: Object
+    }
+}
+</script>
+
+<style>
+</style>
